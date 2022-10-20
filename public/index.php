99a1e4a96ebe4f2740c4d3e4d2b773ffcaf90bd3
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-10-20 04:16:40 +0330

    complete part one

--- public/index.php
+++ public/index.php
@@ -5,4 +5,4 @@ require_once "../vendor/autoload.php";
 $email = new \App\EmailClient(new \App\Email('ysnbgn@yahoo.com'));
 echo $email->connect();
 // echo $email->get();
-
+$email->send(new \App\YahooService());
