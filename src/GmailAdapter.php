230bb7f1430469502332206d23b8d7669cd396ec
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-10-20 05:15:39 +0330

    add gmail adapter to project

---
+++ src/GmailAdapter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+class GmailAdapter extends GmailService implements EmailProvider
+{
+
+    public function connect()
+    {
+        // TODO: Implement connect() method.
+    }
+
+    public function get()
+    {
+        // TODO: Implement get() method.
+    }
+
+    public function send(Email $email)
+    {
+        // TODO: Implement send() method.
+    }
+}
