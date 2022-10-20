8410aacacd192ea2a04a99f731aaf204281e3bdb
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-10-20 03:36:30 +0330

    define methods in yahoo service

--- src/YahooService.php
+++ src/YahooService.php
@@ -2,7 +2,23 @@
 
 namespace App;
 
-class YahooService
+class YahooService implements EmailProvider
 {
 
+    public function connect(Email $email)
+    {
+        return $email;
+    }
+
+    public function get()
+    {
+        $message = "inbox is empty";
+        return $message;
+    }
+
+    public function send(Email $email)
+    {
+        $message = "this message sent.";
+        return $message;
+    }
 }
