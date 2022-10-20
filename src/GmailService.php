45db44cb7a8aaf68ca8f041688fea788e49c2b3e
yasinbaghban <ysnbgn.bgn@gmail.com>
2022-10-20 05:28:41 +0330

    complete gmail service

--- src/GmailService.php
+++ src/GmailService.php
@@ -6,16 +6,19 @@ class GmailService
 {
     public function oAuth()
     {
-
+        $message = "welcome to gmail service";
+        echo $message;
     }
 
     public function sendForNextWeek(Email $email)
     {
-
+        $message = "send message next week";
+        echo $message;
     }
 
     public function getLastWeekInbox()
     {
-
+        $message = "get all message in last week";
+        echo $message;
     }
 }
